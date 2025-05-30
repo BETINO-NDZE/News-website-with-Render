b99762bb0a8a01d1c627544a86572865d62672c9
BETINO-NDZE <150228775+BETINO-NDZE@users.noreply.github.com>
2025-05-30 07:26:44 +0100

    Update connection.php

--- include/connection.php
+++ include/connection.php
@@ -1,15 +1,20 @@
 <?php
+// 
+$host = "dpg-d0sjoak9c44c73f7vevg-a";
+$port = "5432";
+$dbname = "news_portal_db_s0y9";
+$user = "news_portal_db_s0y9_user";
+$password = "xJgAnCJKCC3aF45xHfCsJn1UGedDoFb3"; // Replace with the actual password from Render
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "news";
+// PostgreSQL connection string
+$connStr = "host=$host port=$port dbname=$dbname user=$user password=$password";
 
-$conn = mysqli_connect($servername,$username,$password,$dbname);
-if($conn){
-    // echo "connected";
-}else{
-    die("not connected because ".mysqli_connect_error());
+// Connect to PostgreSQL
+$conn = pg_connect($connStr);
+
+if ($conn) {
+    echo "Connected to PostgreSQL on Render successfully!";
+} else {
+    die("Connection failed: " . pg_last_error());
 }
-
 ?>
